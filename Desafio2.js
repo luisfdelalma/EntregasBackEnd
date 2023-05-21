001e138374e8799907b17acd385fd5ef925c6429
Luis Fernando De La Torre <luisf.dlta@gmail.com>
2023-05-21 07:53:52 -0500

    commit

--- Desafio2.js
+++ Desafio2.js
@@ -1,6 +1,5 @@
 // DESAFIO 2 CORRESPONDIENTE A MANEJO DE DOCUMENTOS CON JAVASCRIPT
 
-const { log } = require('console')
 const fs = require('fs')
 
 class ProductManager {
@@ -47,7 +46,14 @@ class ProductManager {
             let products = JSON.parse(Jproducts)
             products.forEach((prod)=>{ console.log(prod);})
         } else {
-            console.log("No se encontraron productos en la ruta seleccionada");
+            let products = []
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
+            setTimeout(()=>{
+            let Jproducts = fs.readFile(this.path,'utf-8')
+            let products = JSON.parse(Jproducts)
+            products.forEach((prod)=>{ console.log(prod);})
+            }, 2000)
+            
         }
     }
 
@@ -57,22 +63,49 @@ class ProductManager {
             let products = JSON.parse(Jproducts)
 
             let find = products.findIndex((prod)=> prod.id===id)
-            if (find === -1) {
-                console.log("No se encontró ningún producto con el id ingresado");
-            } else {
-                console.log(products[find])
-            }
+
+            find === -1? console.log("No se encontró ningún producto con el id ingresado") : console.log(products[find])
+
         } else {
             console.log("No se ha encontrado productos en la ruta seleccionada");
         }
     }
 
-    // updateProduct (){
+    async updateProduct (id, newInfo){
+        if (fs.existsSync(this.path)) {
+            let Jproducts = await fs.promises.readFile(this.path,'utf-8')
+            let products = JSON.parse(Jproducts)
+            let find = products.findIndex((prod)=> prod.id === id)
+            
+            if (find === -1) {
+                console.log("No se encontró el producto correspondiente al id ingresado");
+            } else {
+                products[find] = {...products[find], ...newInfo}
+                await fs.promises.writeFile(this.path,JSON.stringify(products))
+            }
 
-    // }
+        } else {
+            console.log("No se han encontrado productos en la ruta de archivo seleccionada");
+        }
+    }
+
+    async deleteProduct (id){
+        if (fs.existsSync(this.path)) {
+            let Jproducts =  await fs.promises.readFile(this.path,'utf-8')
+            let products = JSON.parse(Jproducts)
+            let find = products.findIndex((prod)=> prod.id===id)
+            find=== -1 ? console.log("No se han encontrado productos con el id ingresado") : products.splice(find,1)
+            await fs.promises.writeFile(this.path, JSON.stringify(products))
+
+        } else {
+            console.log("Está intentando borrar productos en una ruta inexistente");
+        }
+    }
 }
 
 const producto1 = new ProductManager('./Productos.json')
-// producto1.addProduct('titulo de ejemplo1', 'descripcion de ejemplo1', 98765421, 'sin imagen de ejemplo1', 567231, 6781)
-// producto1.getProducts()
-producto1.getProductsById(1)
+// producto1.addProduct('titulo de ejemplo3', 'descripcion de ejemplo3', 98765423, 'sin imagen de ejemplo3', 567233, 6783)
+producto1.getProducts()
+// producto1.getProductsById(1)
+// producto1.updateProduct(1,{description:"Descripcion actualizada"})
+// producto1.deleteProduct(3)
